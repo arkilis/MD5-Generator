99e269cc6df91967909d860c7503f10cc162a632
Ben <arkilis@gmail.com>
2013-01-14 10:21:42 +1000

    Final Release on MD5 generator

---
+++ MD5/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MD5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.but_addfile = new System.Windows.Forms.Button();
+            this.but_gen = new System.Windows.Forms.Button();
+            this.but_help = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File List:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Progress:";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 52);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(558, 240);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // but_addfile
+            // 
+            this.but_addfile.Location = new System.Drawing.Point(495, 19);
+            this.but_addfile.Name = "but_addfile";
+            this.but_addfile.Size = new System.Drawing.Size(75, 23);
+            this.but_addfile.TabIndex = 4;
+            this.but_addfile.Text = "Add File";
+            this.but_addfile.UseVisualStyleBackColor = true;
+            this.but_addfile.Click += new System.EventHandler(this.but_addfile_Click);
+            // 
+            // but_gen
+            // 
+            this.but_gen.Location = new System.Drawing.Point(12, 343);
+            this.but_gen.Name = "but_gen";
+            this.but_gen.Size = new System.Drawing.Size(75, 23);
+            this.but_gen.TabIndex = 5;
+            this.but_gen.Text = "Generate";
+            this.but_gen.UseVisualStyleBackColor = true;
+            this.but_gen.Click += new System.EventHandler(this.but_gen_Click);
+            // 
+            // but_help
+            // 
+            this.but_help.Location = new System.Drawing.Point(115, 343);
+            this.but_help.Name = "but_help";
+            this.but_help.Size = new System.Drawing.Size(75, 23);
+            this.but_help.TabIndex = 6;
+            this.but_help.Text = "Help";
+            this.but_help.UseVisualStyleBackColor = true;
+            this.but_help.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(87, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(326, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 387);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.but_help);
+            this.Controls.Add(this.but_gen);
+            this.Controls.Add(this.but_addfile);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MD5 Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button but_addfile;
+        private System.Windows.Forms.Button but_gen;
+        private System.Windows.Forms.Button but_help;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ MD5/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace MD5
+{
+    delegate void SetValueCallback(int value);
+
+    public partial class Form1 : Form
+    {
+
+        // global variable
+        public string szMD5Path;
+        public ArrayList ay_list;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ay_list = new ArrayList();
+        }
+
+        // help button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string cmts = "1. First you need to add files that you want to do md5 check;\n";
+            cmts += "2, It will take a while to generate the md5 file which depends on the file size;";
+            MessageBox.Show(cmts);
+            
+        }
+
+        // get file's md5 signature
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            FileStream file = new FileStream(fileName, FileMode.Open);
+            System.Security.Cryptography.MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] retVal = md5.ComputeHash(file);
+            file.Close();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < retVal.Length; i++)
+            {
+                sb.Append(retVal[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+
+        private void but_addfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog1 = new OpenFileDialog();
+            //fileDialog1.InitialDirectory = "d://";
+            //fileDialog1.Filter = "gz files (*.xls)|*.xls|All files (*.*)|*.*";
+            fileDialog1.Filter = "gz files (*.gz)|*.gz";
+            fileDialog1.FilterIndex = 1;
+            fileDialog1.RestoreDirectory = true;
+            if (fileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listView1.Items.Add(fileDialog1.FileName);
+                ay_list.Add(fileDialog1.FileName);
+            }
+
+        }
+
+
+        // generate md5 signature
+        private void but_gen_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+                MessageBox.Show("Please add files");
+            else
+            {
+                // get the path of data.md5
+
+                SaveFileDialog saveFileDialog=new SaveFileDialog();
+                saveFileDialog.FileName = "data.md5";
+                saveFileDialog.Filter="MD5 file|*.md5"; 
+                saveFileDialog.RestoreDirectory=true;
+                if(saveFileDialog.ShowDialog()==DialogResult.OK)
+                    szMD5Path=saveFileDialog.FileName;
+
+                // set md5 job and progress bar
+                progressBar1.Maximum = ay_list.Count;
+
+                Thread t = new Thread(new ThreadStart(doCalMD5));
+                t.Start();
+
+                
+            }
+        }
+
+        // generate md5
+        private void doCalMD5()
+        {
+            if (szMD5Path != "")
+            {
+
+                // create md5 signature file
+                FileStream fs = new FileStream(szMD5Path, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+
+                for (int i = 0; i < ay_list.Count; i++)
+                {
+                    // 
+                    string szMd5Temp = GetMD5HashFromFile((string)ay_list[i]);
+
+                    // update progress bar
+                    SetProcessBarValue(i+1);
+
+                    //MessageBox.Show(szMd5Temp);
+                    string szfilename = System.IO.Path.GetFileName((string)ay_list[i]);
+                    string szTemp = szMd5Temp + "  " + szfilename;
+                    sw.WriteLine(szTemp);
+                    sw.Flush();
+                }
+
+                sw.Close();
+                fs.Close();
+            }
+            else
+                MessageBox.Show("Can't find MD5 file name!");
+        }
+
+        private void SetProcessBarValue(int value)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.progressBar1.InvokeRequired)
+            {
+                SetValueCallback d = new SetValueCallback(SetProcessBarValue);
+                this.Invoke(d, new object[] { value });
+            }
+            else
+            {
+                this.progressBar1.Value = value;
+            }
+        }
+
+    }
+}
